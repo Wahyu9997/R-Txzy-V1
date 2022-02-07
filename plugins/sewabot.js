6c334fc4f9aa8576cc9904bfc21104ef0a02fb0c
Wahyu9997 <96885661+Wahyu9997@users.noreply.github.com>
2022-02-07 08:20:10 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 089516809533
+GoPay: 089516809533
 `.trim()
     const button = {
         buttonText: 'List Harga',
